2c2e9ea60729a7ed3acafc1243dd5c245c8285e7
Gleb Shyshkanov <glebshyshkanov@gmail.com>
2018-04-21 10:05:27 +0300

    Немного ползающего бэка

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour 
+{
+	public float scrollSpeed;
+	public float TileSizeY;
+
+	private Vector3 _startPosition;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, TileSizeY);
+		transform.position = _startPosition + Vector3.up * newPosition;
+	}
+}
