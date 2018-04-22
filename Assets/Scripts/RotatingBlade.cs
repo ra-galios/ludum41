0661018dd72bf6af02b99a409bec8fa37529c957
Gleb Shyshkanov <glebshyshkanov@gmail.com>
2018-04-22 17:21:02 +0300

    brought scene objects to life

---
+++ Assets/Scripts/RotatingBlade.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+public class RotatingBlade : MonoBehaviour
+{
+	public float RotationSpeed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Rigidbody2D>().angularVelocity = RotationSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		GameObject otherObj = other.gameObject;
+		if(otherObj.CompareTag("Spirit"))
+			Destroy(otherObj);
+	}
+}
---
+++ Assets/TrapController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+	private GameObject child;
+	public float Delay;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		child = transform.GetChild(0).gameObject;
+
+		if (Delay < 0.1)
+		{
+			child.SetActive(true);
+		}
+		else
+		{
+			StartCoroutine(RunTrap());
+		}
+	}
+
+	private void FlipVisibility()
+	{
+		child.SetActive(!child.activeSelf);	
+	}
+
+	IEnumerator RunTrap()
+	{
+		while (true)
+		{
+			FlipVisibility();
+			yield return new WaitForSeconds(Delay);
+		}
+	}
+	
+}
