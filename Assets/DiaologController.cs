d399c474aee9250d42bd86ce5e10875252de8929
Gleb Shyshkanov <glebshyshkanov@gmail.com>
2018-04-23 18:52:35 +0300

    Добавлен стартовый диалог

---
+++ Assets/DiaologController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DiaologController : MonoBehaviour
+{
+	public Text RepliqueField;
+	public Text ButtonText;
+
+	private int _replique = -1;
+	
+	private string[][] _dialogs = new string[][]
+	{
+		new String[]{"Wandering this land obscure and lonely, you’ve met a mysterious woman", "Next"},
+		new String[]{"Woman: I've been waiting for you for too long…", "Next"},
+		new String[]{"Woman: For the Spirits that walk in the shadows you are the last hope", "Why do you even care about those lost souls?"},
+		new String[]{"Woman: Because you never know who they were in their previous life… Or who they might become", "Start"}
+	};
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Next();
+	}
+
+	public void Next()
+	{
+		_replique++;
+		if (_dialogs.Length > _replique)
+		{
+			string[] texts = _dialogs[_replique];
+			RepliqueField.text = texts[0];
+			ButtonText.text = texts[1];
+		}
+		else
+		{
+			startGame();
+		}
+	}
+
+	private void startGame()
+	{
+		SceneManager.LoadScene("TestScene_halina_lastday");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
